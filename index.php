fc5bcde7e00559421f5d18af9963df6e0953be13
ssekaibrahim <ssekaibrahim@gmail.com>
2024-05-11 00:09:15 +0300

    update

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ibrahim SSeka | UX Front-End Developer</title>
+    <link rel="stylesheet" href="app.css">
+<meta name="description" content="">
+</head>
+<body>
+    
+    <section>
+        <div class="container">
+            <h1>UX Designer, Front-End Developer</h1>
+            <p>I am Ibrahim Sseka a passionate UX designer and front-end developer based in Kampala, Uganda</p>
+        </div>
+    </section>
+</body>
+</html>
